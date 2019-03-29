e264b6e54bf3d873ff0a74936850a146a9174385
Bianca Galvan <bianca_galvan@me.com>
2019-03-29 16:40:23 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // add solution here
+function theBeatlesPlay(musicians,instrument){
+  var allMusicians=[]
+  for(var i=0;i<musicians.length;i++){
+    allMusicians.push(musicians[i] + ' plays ' + instrument[i])
+  }
+  return allMusicians;
+}
+
+function johnLennonFacts(facts){
+    var newFacts=[]
+    let i=0;
+    while(i<facts.length){
+      newFacts.push(facts[i]+"!!!")
+      i++
+    }
+    return newFacts;
+}
+
+function iLoveTheBeatles(num) {
+    var love = [];
+    do {
+        love.push("I love the Beatles!");
+        num++;
+    } while (num < 15);
+
+    return love;
+}
